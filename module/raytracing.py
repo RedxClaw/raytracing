fe19b18c12006c82598ec27ef4aaeacbad5c4d67
RedxClaw <RedxClaw>
2025-10-20 15:24:46 +0200

    Update raytracing.py

--- module/raytracing.py
+++ module/raytracing.py
@@ -4,20 +4,30 @@ import jax.numpy as jnp
 import numpy as np
 from numpy.linalg import norm
 import matplotlib.pyplot as plt
-import math
 
-def intersection(F, x, ball):
-    v = x - F
-    v = v/norm(v)
-    d = ball[0]
+def solution_intersection(cam_pos, v, sphere):
+    d = sphere[0] # Position du centre
+    r = sphere[1] # Rayon de la sphère
 
     a = np.dot(v, v)
-    b = 2*np.dot(v, F - d)
-    c = (np.dot(F - d, F - d) - ball[1]**2)
+    b = 2 * np.dot(v, cam_pos - d)
+    c = np.dot(cam_pos - d, cam_pos - d) - r*r
 
     delta = b*b - 4*a*c
 
-    if delta >= 0:
-        if (-b + math.sqrt(delta))/(2*a) > 0:
-            return 1
-    return 0
+    solution = []
+
+    if delta == 0:
+        s = -b/(2*a)
+        if s > 0 : solution.append(s)
+
+    elif delta > 0: 
+        s = -b + np.sqrt(delta)/(2*a)
+        if s > 0 : solution.append(s)
+        s = -b - np.sqrt(delta)/(2*a)
+        if s > 0 : solution.append(s)
+
+    elif delta < 0: 
+        solution = None
+
+    return solution
